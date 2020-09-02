b910b2b23a49ef7975370dce3a99a3a23f58e23c
niggo-edw <wunderwitz@gmx.net>
2020-09-02 22:14:30 +0200

    Created site

---
+++ script.js
@@ -0,0 +1,10 @@
+function getRandomSite(){
+    var result = '';
+    var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 6; i++ ) {
+       result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    url = "https://prntscr.com/" + result;
+    window.open(url);
+}
