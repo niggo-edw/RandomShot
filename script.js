91e41d25991024f2fd3c0bcaa48961623ab53f0e
niggo-edw <70602568+niggo-edw@users.noreply.github.com>
2020-09-03 13:19:48 +0200

    Button now opens 5 tabs

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 function getRandomSite(){
-    var result = '';
-    var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-    var charactersLength = characters.length;
-    for ( var i = 0; i < 6; i++ ) {
-       result += characters.charAt(Math.floor(Math.random() * charactersLength));
-    }
-    url = "https://prntscr.com/" + result;
-    window.open(url);
+    for ( var c = 0; c < 5; c++){
+        var result = '';
+        var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+        var charactersLength = characters.length;
+        for ( var i = 0; i < 6; i++ ) {
+           result += characters.charAt(Math.floor(Math.random() * charactersLength));
+        }
+        url = "https://prntscr.com/" + result;
+        window.open(url);
+    };
 }
