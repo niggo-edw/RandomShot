db38b9d1e8b590eefcb62d23d82bb59a4ce9bd9e
niggo-edw <70602568+niggo-edw@users.noreply.github.com>
2021-03-18 17:36:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
 function getRandomSite(){
-    for ( var c = 0; c < 5; c++){
-        var result = '';
-        var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
-        var charactersLength = characters.length;
-        for ( var i = 0; i < 6; i++ ) {
-           result += characters.charAt(Math.floor(Math.random() * charactersLength));
-        }
-        url = "https://prntscr.com/" + result;
-        window.open(url);
-    };
+    var result = '';
+    var characters = 'abcdefghijklmnopqrstuvwxyz0123456789';
+    var charactersLength = characters.length;
+    for ( var i = 0; i < 6; i++ ) {
+        result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    }
+    url = "https://prntscr.com/" + result;
+    window.open(url);
 }
